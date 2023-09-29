ca10eac165b30b39c17e3fccad3bbbf3851765cf
pramod sharma <pramod.sharma@mindpooltech.in>
2023-09-29 14:07:25 +0530

    added call function in databse seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CitySeeder;
+use Database\Seeders\BrandSeeder;
+use Database\Seeders\StateSeeder;
+use Database\Seeders\ProductSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([BrandSeeder::class, ProductSeeder::class, StateSeeder::class, CitySeeder::class]);
     }
 }
